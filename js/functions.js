c86bb5b80aff8b02d6d5bf3cc668b7f8e9d96bfe
Gusarich <danielsedovzzz@gmail.com>
2022-06-06 16:59:02 +0800

    add reset and game is actually playable now

--- js/functions.js
+++ js/functions.js
@@ -15,6 +15,16 @@ function format (n) {
 
 function action () {
     // gameValues.prob = gameValues.rtp / (100 * gameValues.mul)
+    if (gameValues.r >= 1) {
+        if (rng.random() < gameValues.prob) gameValues.r *= gameValues.mul
+        else gameValues.r = 0
+        gameValues.x += 1
+    }
+}
+
+function reset () {
+    gameValues.r = gameValues.base_r
+    $('#random_title')[0].innerText = randomString()
 }
 
 function update () {
@@ -22,5 +32,6 @@ function update () {
     $('#value_x')[0].innerText = format(gameValues.x)
     $('#value_rtp')[0].innerText = format(gameValues.rtp) + '%'
     $('#value_mul')[0].innerText = 'x' + format(gameValues.mul)
-    $('#value_prob')[0].innerText = format(gameValues.prob) + '%'
+    $('#value_prob')[0].innerText = format(gameValues.prob * 100) + '%'
+    $('#value_base_r')[0].innerText = format(gameValues.base_r)
 }
--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ $(document).ready(() => {
     window.gameValues = {
         r: 1.0,
         x: 0.0,
-        rtp: 90.0,
+        rtp: 80.0,
         mul: 2.0,
-        prob: 0.45,
+        prob: 0.4,
+        base_r: 1.0,
     }
 
     $('#actions_action').click(action)
+    $('#actions_reset').click(reset)
 
     setInterval(update, 25)
 })
